98744705719cfd4bc9a301fedf3a46d1ff23aab2
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2022-04-13 23:48:37 +0200

    Initial commit, SettingActivity, changes to layout and more.

--- app/src/main/java/fi/arcada/sos_projekt_chart_sma/MainActivity.java
+++ app/src/main/java/fi/arcada/sos_projekt_chart_sma/MainActivity.java
@@ -2,8 +2,14 @@ package fi.arcada.sos_projekt_chart_sma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -18,22 +24,50 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    String currency, datefrom, dateto;
+    TextView titleText, statsText;
+    Button button1, button2;
+    String currency, datefrom, dateto, currencyChoice, fromDate, toDate,smaWin;
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TEMPORÄRA VÄRDEN
-        currency = "USD";
-        datefrom = "2022-01-01";
-        dateto = "2022-02-01";
+        try {
 
-        // Hämta växelkurser från API
-        ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
-        // Skriv ut dem i konsolen
-        System.out.println(currencyValues.toString());
+            sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+            currencyChoice = sharedPref.getString("currencyChoice", "EUR");
+            fromDate = sharedPref.getString("fromDate", "2022-01-01");
+            toDate = sharedPref.getString("toDate", "2022-02-01");
+            smaWin = sharedPref.getString("windowSize", "3");
+
+            // TEMPORÄRA VÄRDEN
+            currency = currencyChoice;
+            datefrom = fromDate;
+            dateto = toDate;
+
+            titleText = findViewById(R.id.titleText);
+            statsText = findViewById(R.id.statsText);
+            button1 = findViewById(R.id.button1);
+            button2 = findViewById(R.id.button2);
+
+            titleText.setText("Exchange rate - " + currencyChoice);
+            statsText.setText(currency + " | " + fromDate + " - " + toDate);
+
+            // Hämta växelkurser från API
+            ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
+            // Skriv ut dem i konsolen
+            System.out.println(currencyValues.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void settingsClick(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
 
